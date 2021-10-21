7a01014fbdf613285a9690b9d4a888466bb5e3d2
Fábio Augusto Pandolfo <fabio@digitaldoc.com.br>
2021-10-21 15:10:26 -0300

    fixed invoker and wrapper

--- src/cmd/System/slb-invker.cmd
+++ src/cmd/System/slb-invker.cmd
@@ -17,7 +17,9 @@ SETLOCAL
 CALL slb-helper "%~f0" "%~1" >NUL
 IF DEFINED -help SET -args=-arg:%-help%
 IF NOT DEFINED -help SET -args='%*'
+IF "%-args%"=="''" SET "-args="
 SET -script=-sh:"%~dp0%0.sh"
+SET -script=%-script:src\cmd=src\sh%
 CALL slb-wrappr %-script% %-args%
 
 ENDLOCAL & GOTO :eof
